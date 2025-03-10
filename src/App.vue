e70a16f4233c32204a636c86b5760b81cb3d0570
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-03-10 14:52:53 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <header>
+    <NavBar />
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style>
+@reference "tailwindcss";
+
+header {
+  @apply max-w-screen px-4 py-8;
+}
+
+main {
+  @apply w-screen overflow-auto;
+}
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink
+      to="/"
+    >Produtos</RouterLink>
+  </nav>
+</template>
+
+<style>
+  @reference "tailwindcss";
+
+  .router-link-exact-active {
+    @apply text-green-700;
+  }
+
+  nav {
+    @apply w-full flex justify-around;
+  }
+</style>
